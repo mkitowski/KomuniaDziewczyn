e5c434328513e0ea7159b92b4724f18c4056be4f
mkitowski <maciej.kitowski@gmail.com>
2019-05-21 21:22:22 +0200

    gallery controls added

--- src/components/GalleryPage/GalleryPage.js
+++ src/components/GalleryPage/GalleryPage.js
@@ -35,6 +35,7 @@ const StyledDiv = styled.div`
       width: 100%
       transition: all .4s ease-in-out;
       vertical-align: middle;
+      
       :hover {
         transform: scale(1.2);
       }
@@ -43,23 +44,73 @@ const StyledDiv = styled.div`
 `;
 
 class GalleryPage extends React.Component {
+  state = {
+    pictureVisible: false,
+    picNumber: 0
+  }
+
+  clickHandlePicture = event => {
+    this.setState({
+      pictureVisible: true,
+      picNumber: event.target.alt
+    });
+  }
+
+  clickClosePicture = () => {
+    this.setState({
+      pictureVisible: false
+    })
+  }
+
+  next = () => {
+    if(this.state.picNumber === this.props.filesNames.length-1){
+      this.setState({
+        picNumber: 0
+      })
+    } else {
+      this.setState({
+        picNumber: +this.state.picNumber+1
+      })
+    }
+  }
+
+  prev = () => {
+    if (this.state.picNumber === 0) {
+      this.setState({
+        picNumber: this.props.filesNames.length-1
+      })
+    } else {
+      this.setState({
+        picNumber: +this.state.picNumber - 1
+      })
+    }
+  }
+
+  pressed = event => {
+    console.log(event);
+  }
 
   render(){
     const files = this.props.filesNames || [];
     return (
-      <StyledDiv>
-        <Grid container spacing={24} className={"gallery"}>
+      <StyledDiv >
+        <Grid container spacing={24} className={"gallery"}  >
           {
             files.map((el, i) => {
               return (
-                <Grid key={i} item xs={3} className={'photoContainer'}>
+                <Grid key={i} item xs={3} className={'photoContainer'} onClick={this.clickHandlePicture}>
                   <img src={el} alt={i} />
                 </Grid>
               )
             })
           }
         </Grid>
-        {false && <Picture file={files[22]}/>}
+        {this.state.pictureVisible && 
+          <Picture file={files[this.state.picNumber]} 
+            close={this.clickClosePicture} 
+            next={this.next} 
+            prev={this.prev}
+          />}
       </StyledDiv>
     );
   }
--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -9,21 +9,84 @@ const StyledDiv = styled.div`
   height: 100vh;
   background-color: rgba(0,0,0,.5);
   z-index: 500;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  img {
-    max-width: 90%;
-    max-height: 90%;
+  
+  .center{
+    position: fixed;
+    top: 2.5%;
+    left: 5%;
+    width: 90%;
+    height: 95%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      z-index: 503;
+    }
+  }
+  .left{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 15%;
+    height: 100vh;
+    :hover {
+      background-color: rgba(255,255,255,.2);
+      cursor: pointer;
+    }
+  }
+  .right{
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 15%;
+    height: 100vh;
+    :hover {
+      background-color: rgba(255,255,255,.2);
+      cursor: pointer;
+    }
   }
 `;
 
-const Picture = ({file}) =>{
-  return(
-    <StyledDiv>
-      <img src={file} alt={file} />
-    </StyledDiv>
-  )
-}
+
+
+class Picture extends React.Component{
+
+
+  componentDidMount() {
+    document.body.addEventListener('keyup', this.keyPress);
+  }
+
+  keyPress = e => {
+    
+      switch (e.keyCode) {
+        case 39:
+          return this.props.next();
+        case 37:
+          return this.props.prev();
+        case 27:
+          return this.props.close();
+        default:
+          return;
+      }
+    
+  }
+
+  componentWillUnmount(){
+    document.body.removeEventListener('keyup', this.keyPress);
+  }
+
+  render() {
+
+    return (
+      <StyledDiv>
+        <div className={'center'}><img src={this.props.file} alt={this.props.file} onClick={this.props.close}/></div>
+        <div className={'left'} onClick={this.props.prev} ></div>
+        <div className={'right'} onClick={this.props.next} ></div>
+      </StyledDiv>
+    )
+  }
+} 
 
 export default Picture;
